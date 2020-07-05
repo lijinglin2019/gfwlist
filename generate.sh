e43633b30b2c54080c73c7e1b9b6fe0f172e684e
lijinglin <me@lijinglin.dev>
2020-07-05 14:26:45 +0800

    rename to proxylist

--- generate.sh
+++ generate.sh
@@ -4,17 +4,24 @@ function error() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
 }
 
-GFWLIST=$(curl -s https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt | base64 -d)
+GFW_LIST_BASE64=$(curl -s https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt)
 if [[ $? != 0 ]]; then
     error "get gfwlist failed"
     exit -1
 fi
 
-PROXY=$(cat proxy.list | sort | uniq)
+GFW_LIST=$(echo "$GFW_LIST_BASE64" | base64 -d)
+if [[ $? != 0 ]]; then
+    error "decode gfwlist failed"
+    exit -1
+fi
+
+PROXY_LIST=$(cat proxy.list | sort | uniq)
 if [[ $? != 0 ]]; then
     error "get proxylist failed"
     exit -1
 fi
+PROXY_LIST=$(echo "$PROXY_LIST" | sort | uniq)
 
-echo -e "$GFWLIST\n\n$PROXY" | base64 >gfwlist.txt
-echo "$PROXY" >proxy.list
+echo -e "$GFW_LIST\n\n$PROXY_LIST" | base64 >proxylist.txt
+echo "$PROXY_LIST" >proxy.list
