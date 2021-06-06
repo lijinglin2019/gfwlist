f93f22e26ab6d6721d63870321ad4e9f16b4c54e
lijinglin <me@lijinglin.dev>
2021-06-06 19:16:16 +0800

    generate proxylist.txt by v2ray domain list

--- main.go
+++ main.go
@@ -13,31 +13,16 @@ import (
 )
 
 func main() {
-	if err := genSurgeRules(); err != nil {
-		log.Fatalln(err)
-	}
-	if err := genProxyList(); err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func genSurgeRules() error {
-	v2rayDomainList, err := loadV2rayDomainList()
+	list, err := loadV2rayDomainList()
 	if err != nil {
-		return err
+		log.Fatalln(err)
 	}
-	for _, entry := range v2rayDomainList.Entry {
-		if strings.HasPrefix(entry.CountryCode, "CATEGORY") ||
-			strings.HasPrefix(entry.CountryCode, "GEO") ||
-			entry.CountryCode == "CN" {
-			data := parseV2rayDomainListToSurgeList(entry.Domain)
-			err = ioutil.WriteFile("./surge/"+strings.ToLower(entry.CountryCode), data, 0644)
-			if err != nil {
-				return fmt.Errorf("failed to generate surge rule %s", strings.ToLower(entry.CountryCode))
-			}
-		}
+	if err := genSurgeRules(list); err != nil {
+		log.Fatalln(err)
 	}
-	return nil
+	if err := genProxyList(list); err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func loadV2rayDomainList() (*router.GeoSiteList, error) {
@@ -58,48 +43,54 @@ func loadV2rayDomainList() (*router.GeoSiteList, error) {
 	return list, nil
 }
 
-func parseV2rayDomainListToSurgeList(domains []*router.Domain) []byte {
-	ret := make([]byte, 0)
-	for _, domain := range domains {
-		switch domain.Type {
-		case router.Domain_Plain:
-			ret = append(ret, []byte("DOMAIN-KEYWORD,"+domain.Value+"\n")...)
-		case router.Domain_Regex:
-			ret = append(ret, []byte("URL-REGEX,"+domain.Value+"\n")...)
-		case router.Domain_Domain:
-			ret = append(ret, []byte("DOMAIN-SUFFIX,"+domain.Value+"\n")...)
-		case router.Domain_Full:
-			ret = append(ret, []byte("DOMAIN,"+domain.Value+"\n")...)
-		default:
+func genSurgeRules(list *router.GeoSiteList) error {
+	for _, entry := range list.Entry {
+		if strings.HasPrefix(entry.CountryCode, "CATEGORY-") ||
+			strings.HasPrefix(entry.CountryCode, "GEOLOCATION-") ||
+			strings.HasPrefix(entry.CountryCode, "TLD-") ||
+			entry.CountryCode == "CN" {
+			data := make([]byte, 0)
+			for _, domain := range entry.Domain {
+				switch domain.Type {
+				case router.Domain_Plain:
+					data = append(data, []byte("DOMAIN-KEYWORD,"+domain.Value+"\n")...)
+				case router.Domain_Regex:
+					data = append(data, []byte("URL-REGEX,"+domain.Value+"\n")...)
+				case router.Domain_Domain:
+					data = append(data, []byte("DOMAIN-SUFFIX,"+domain.Value+"\n")...)
+				case router.Domain_Full:
+					data = append(data, []byte("DOMAIN,"+domain.Value+"\n")...)
+				}
+			}
+			err := ioutil.WriteFile("surge/"+strings.ToLower(entry.CountryCode), data, 0644)
+			if err != nil {
+				return fmt.Errorf("failed to generate surge rule %s", strings.ToLower(entry.CountryCode))
+			}
 		}
 	}
-	return ret
+	return nil
 }
 
-func genProxyList() error {
-	gfwList, err := loadGfwList()
-	if err != nil {
-		return err
+func genProxyList(list *router.GeoSiteList) error {
+	src := make([]byte, 0)
+	for _, entry := range list.Entry {
+		prefix := ""
+		if entry.CountryCode == "CN" {
+			prefix = "@@||"
+		} else if entry.CountryCode == "GEOLOCATION-!CN" {
+			prefix = "||"
+		} else {
+			continue
+		}
+		for _, domain := range entry.Domain {
+			src = append(src, []byte(prefix+domain.Value+"^\n")...)
+		}
 	}
-	proxyList, err := ioutil.ReadFile("proxy.list")
-	if err != nil {
-		return err
-	}
-	src := append(gfwList, proxyList...)
 	data := make([]byte, base64.StdEncoding.EncodedLen(len(src)))
 	base64.StdEncoding.Encode(data, src)
-	return ioutil.WriteFile("proxylist.txt", data, 0644)
-}
-
-func loadGfwList() ([]byte, error) {
-	resp, err := http.Get("https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt")
+	err := ioutil.WriteFile("proxylist.txt", data, 0644)
 	if err != nil {
-		return nil, fmt.Errorf("failed to download gfwlist.txt, err: %+v", err)
+		return fmt.Errorf("failed to generate proxy list")
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read gfwlist.txt, err: %+v", err)
-	}
-	return base64.StdEncoding.DecodeString(string(body))
+	return nil
 }
